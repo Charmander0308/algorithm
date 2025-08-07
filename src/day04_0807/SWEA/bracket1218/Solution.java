39284e4453a023e39be95c8733253956164e2cf3
droppord <droppord@naver.com>
2025-08-07 17:09:17 +0900

    4일차 멀캠

---
+++ src/day04_0807/SWEA/bracket1218/Solution.java
@@ -0,0 +1,65 @@
+package day04_0807.SWEA.bracket1218;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File file = new File("src/day04_0807/SWEA/bracket1218/input.txt");
+		Scanner sc = new Scanner(file);
+		int T = 10;
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			int total = sc.nextInt();
+			String input = sc.next();
+			Stack<Character> stack = new Stack<>();
+			
+			//일단 다 맞다고 가정하자. 그리고 아닌걸 찾자 
+			int isOk = 1;
+			
+			for(int i = 0; i < total; i++) {
+				//여는 괄호면 푸쉬
+				if(input.charAt(i) == '{' || input.charAt(i) == '[' || input.charAt(i) == '(' || input.charAt(i) == '<') {
+					stack.push(input.charAt(i));
+				} else {	//닫는 괄호면 항아리 가장 위 내용물 확인
+					if(input.charAt(i) == '}') {
+						if(stack.peek() == '{') {	//규칙에 맞으면 무사통과 + 짝 맞춰서 내용물 제거
+							stack.pop();
+						} else {
+							isOk = 0;	//규칙에 위배되면 isOk = 0이고 더이상 검사할 필요없으니 for문 탈출, 이하 동일
+							break;
+						}
+					} else if(input.charAt(i) == ']') {
+						if(stack.peek() == '[') {
+							stack.pop();
+						} else {
+							isOk = 0;
+							break;
+						}
+					} else if(input.charAt(i) == ')') {
+						if(stack.peek() == '(') {
+							stack.pop();
+						} else {
+							isOk = 0;
+							break;
+						}
+					} else if(input.charAt(i) == '>') {
+						if(stack.peek() == '<') {
+							stack.pop();
+						} else {
+							isOk = 0;
+							break;
+						}
+					}
+				}
+			}
+			System.out.println("#" + test_case + " " + isOk);
+			
+		}
+		
+	}
+
+}
---
+++ src/day04_0807/SWEA/zero8931/Solution.java
@@ -0,0 +1,45 @@
+package day04_0807.SWEA.zero8931;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("src/day04_0807/SWEA/zero8931/sample_input.txt");
+		Scanner sc = new Scanner(file);
+		int T = sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			int K = sc.nextInt();
+			//항아리 1개
+			Stack<Integer> stack = new Stack<>();
+			
+			//K번 넣거나 빼기
+			for(int i = 0; i < K; i++) {
+				int input = sc.nextInt();
+				//0이 호출되면
+				if(input == 0) {
+					if(!stack.isEmpty()) {	//항아리가 비어있지 않으면 직전 값 빼기
+						stack.pop();
+					} else {	//비어있는데 더 빼려고하면
+						System.out.println("뺄게없음");
+						continue;
+					}
+				} else {	//그 외엔 스택 쌓기
+					stack.push(input);
+				}
+			}
+			//초기값은 0
+			int ans = 0;
+			while(!stack.isEmpty()) {	//비어있지 않다면 빌때까지 ans에 계속 더해주기
+				ans += stack.pop();
+			}
+			//완성
+			System.out.println("#" + test_case + " " + ans);
+			
+		}
+		sc.close();
+	}
+}
