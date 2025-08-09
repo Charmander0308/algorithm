ee433d1669ebfda64c310e023b71d42a57b305a5
yesung han <yesunghan@yesungui-MacBookPro.local>
2025-08-10 00:01:55 +0900

    20250809home

---
+++ src/weekend01_0809/baekjoon/ball10813/Main.java
@@ -0,0 +1,34 @@
+package weekend01_0809.baekjoon.ball10813;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		//바구니 
+		int[] basket = new int[N];
+		//번호에 맞게 공을 넣기 
+		for(int i=0; i < N; i++) {
+			basket[i] = i + 1;
+		}
+		//두 수를 받아서 해당 번호 바구니의 공을 교환 
+		for(int i=0; i < M; i++) {
+			int input1 = sc.nextInt();
+			int input2 = sc.nextInt();
+			
+			int tmp = basket[input1 - 1];
+			basket[input1 - 1] = basket[input2 - 1];
+			basket[input2 - 1] = tmp;
+		}
+		//최종 결과 
+		for(int i = 0; i < N; i++) {
+			System.out.print(basket[i]);
+			if(i < N-1) {
+				System.out.print(" ");
+			}
+		}
+		sc.close();
+	}
+}
