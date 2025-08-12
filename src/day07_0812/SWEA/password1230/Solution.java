df4f574a2185358bdc9bae887962efbd5784c433
yesung han <yesunghan@yesungui-MacBookPro.local>
2025-08-13 00:57:22 +0900

    20250812home

--- src/day07_0812/SWEA/password1230/Solution.java
+++ src/day07_0812/SWEA/password1230/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
 	public static void main(String[] args) throws FileNotFoundException {
 		File file = new File("src/day07_0812/SWEA/password1230/input.txt");
 		Scanner sc = new Scanner(file);
-		int T = sc.nextInt();
+		int T = 10;
 		
 		for(int test_case = 1; test_case <= T; test_case++) {
 			//원본 암호문 뭉치 속 암호문의 개수
@@ -24,26 +24,40 @@ public class Solution {
 			int M = sc.nextInt();
 			
 			//명령어
-			List<String> commands = new LinkedList<>(); 
-			for(int i = 0; i < M; i++) {
-				commands.add(sc.next());
-			}
-			
-			for(int i = 0; i < M; i++) {
-				String s = commands.get(i);
-				if(s.equals("I")) {
-					
+			for(int i=0; i < M; i++) {
+				//어떤 명령어인가? 
+				String order = sc.next();
+				//인덱스와 암호 수를 받아서 반복문으로 다음 문자열들 받아서 해당 위치에 삽입 
+				if(order.equals("I")) {
+					int index = sc.nextInt();
+					int count = sc.nextInt();
+					for(int j = 0; j < count; j++) {
+						passwords.add(index + j, sc.next());
+					}
 				}
-				
+				//인덱스와 암호 수를 받아서 해당 위치에서 수만큼 삭제 
+				else if(order.equals("D")) {
+					int index = sc.nextInt();
+					int count = sc.nextInt();
+					for(int j = 0; j < count; j++) {
+						passwords.remove(index);
+					}
+				}
+				//수량을 받고, 문자열들을 받아 리스트 맨 뒤에 붙인다 
+				else if(order.equals("A")){
+					int count = sc.nextInt();
+					for(int j = 0; j < count; j++) {
+						passwords.add(sc.next());
+					}
+				}
 			}
-			
-			
-			
-			
-			
-			
-			
-			
+			//나오세요~ 
+			System.out.print("#" + test_case);
+			for(int i = 0; i < 10; i++) {
+				System.out.print(" "+passwords.get(i));
+			}
+			System.out.println();
+				
 			
 		}
 		
