d33f20362e310a857e63dc7a183b8ca61bfb1ab5
Charmander0308 <dtdt4959@gmail.com>
2025-08-16 11:10:45 +0900

    20250815home2

--- src/day09_0814/SWEA/crop2805/Solution.java
+++ src/day09_0814/SWEA/crop2805/Solution.java
@@ -2,7 +2,9 @@ package day09_0814.SWEA.crop2805;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Solution {
 	public static void main(String[] args) throws FileNotFoundException {
@@ -11,6 +13,23 @@ public class Solution {
 		int T = sc.nextInt();
 		
 		for(int test_case = 1; test_case <= T; test_case++) {
+			int N = sc.nextInt();
+			sc.nextLine();
+			
+			int center = N/2+1;
+			int[][] farm = new int[N][N];
+			
+			//숫자가 띄어쓰기가 없네요
+			for(int i = 0; i < N; i++) {
+				String line = sc.nextLine();
+				for(int j = 0; j < N; j++) {
+					farm[i][j] = line.charAt(j) -'0';
+				}
+			}
+			
+			
+			
+					
 			
 			
 			
