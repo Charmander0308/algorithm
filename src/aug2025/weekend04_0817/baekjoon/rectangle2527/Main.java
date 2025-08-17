141e4c43b8e6fbfe2acd8b6abc9fe37320af3d49
yesung han <yesunghan@yesungui-MacBookPro.local>
2025-08-17 21:06:00 +0900

    20250817home

---
+++ src/aug2025/weekend04_0817/baekjoon/rectangle2527/Main.java
@@ -0,0 +1,15 @@
+package aug2025.weekend04_0817.baekjoon.rectangle2527;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+	}
+
+}
