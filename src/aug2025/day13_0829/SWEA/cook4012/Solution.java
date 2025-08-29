275d5417c37e2a18c7847efd6f3e387ca7fd6c34
Charmander0308 <dtdt4959@gmail.com>
2025-08-29 17:51:41 +0900

    20250829campus

---
+++ src/aug2025/day13_0829/SWEA/cook4012/Solution.java
@@ -0,0 +1,33 @@
+package aug2025.day13_0829.SWEA.cook4012;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("src/aug2025/day13_0829/SWEA/cook4012/sample_input.txt");
+		Scanner sc = new Scanner(file);
+		int T = sc.nextInt();
+		
+		for(int test_case=1; test_case <=T; test_case++) {
+			int N = sc.nextInt();
+			int M = 2;
+			
+			combination(0, 0);
+			
+			
+			
+			
+		}
+		
+	}
+
+	public static void combination(int idx, int sidx) {
+		
+		
+	}
+	
+	
+
+}
---
+++ src/aug2025/day13_0829/SWEA/russia4613/Solution.java
@@ -0,0 +1,9 @@
+package aug2025.day13_0829.SWEA.russia4613;
+
+public class Solution {
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
