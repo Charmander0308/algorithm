7b37de20c3d0278b481519cd8ecefea75d1bd02e
Charmander0308 <dtdt4959@gmail.com>
2025-09-03 18:27:32 +0900

    20250903campus

---
+++ src/sep2025/day16_0903/SWEA/tree14510/Solution.java
@@ -0,0 +1,14 @@
+package sep2025.day16_0903.SWEA.tree14510;
+
+import java.io.File;
+
+public class Solution {
+	public static void main(String[] args) {
+		File file = new File("src/sep2025/day16_0903/SWEA/tree14510/sample_input.txt");
+		
+		
+		
+		
+	}
+
+}
