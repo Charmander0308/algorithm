c48c8e6595890d04eaad551c37f96e6c4adc672d
Charmander0308 <dtdt4959@gmail.com>
2025-09-15 23:18:48 +0900

    20250915home

--- src/sep2025/day23_0915/BOJ/nm15651/Main.java
+++ src/sep2025/day23_0915/BOJ/nm15651/Main.java
@@ -1,37 +1,40 @@
 package sep2025.day23_0915.BOJ.nm15651;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int N;
 	static int M;
 	static int[] arr;
 	static int[] ans;
+	static StringBuilder sb = new StringBuilder();
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		arr = new int[N+1];
-		for(int i=0; i<=N; i++) {
-			arr[i] = i;
-		}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
 		ans = new int[M];
-		bt(0, 0);
+		bt(0);
+		System.out.println(sb);
 	}
 	
-	static void bt(int num, int idx) {
+	static void bt(int idx) {
 		if(idx == M) {
 			for(int i : ans) {
-				System.out.print(i + " ");
+				sb.append(i).append(" ");
 			}
-			System.out.println();
+			sb.append("\n");
 			return;
 		}
 		
 		for(int i=1; i<=N; i++) {
-			ans[num] = i;
-			bt(num+1, idx+1);
+			ans[idx] = i;
+			bt(idx+1);
 		}
 	}
 }
