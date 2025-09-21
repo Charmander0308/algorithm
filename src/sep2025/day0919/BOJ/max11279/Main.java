d4cd9bcd40ec70169171b144b2af7eb6fd941795
Charmander0308 <dtdt4959@gmail.com>
2025-09-21 22:29:36 +0900

    20250921home

--- src/sep2025/day0919/BOJ/max11279/Main.java
+++ src/sep2025/day0919/BOJ/max11279/Main.java
@@ -1,40 +1,33 @@
 package sep2025.day0919.BOJ.max11279;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
-	static int[] heap;
+	static PriorityQueue<Integer> pq;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		pq = new PriorityQueue<>(Collections.reverseOrder());
 		int N = sc.nextInt();
 		
 		for(int i=0; i<N; i++) {
 			int num = sc.nextInt();
-			heap = new int[N];
+			//0이면 출력
 			if(num == 0) maxPrint();
-			else addNum(num);
+			//자연수면 힙에 저장
+			else pq.add(num);
 		}
-		
 		
 	}
 
 	private static void maxPrint() {
-		if(heap[0] == 0) {
+		if(pq.isEmpty()) {
 			System.out.println(0);
 		}
 		else {
-			System.out.println(heap[0]);
+			System.out.println(pq.poll());
 		}
 	}
-	
-	private static void addNum(int num) {
-		if(heap[0] == 0) {
-			
-		}
-		else {
-			
-		}
-	}
-
 }
