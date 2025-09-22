40bfd4997d598f44580918337d606e100c7251a4
Charmander0308 <dtdt4959@gmail.com>
2025-09-22 17:56:30 +0900

    20250922campus

---
+++ src/sep2025/day30_0922/BOJ/decrease1038/Main.java
@@ -0,0 +1,11 @@
+package sep2025.day30_0922.BOJ.decrease1038;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+	}
+}
