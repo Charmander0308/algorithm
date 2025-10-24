fafbcb1577b63b7d4c34e2c25dc146b071571d32
Charmander0308 <dtdt4959@gmail.com>
2025-10-24 09:52:46 +0900

    20251024campus

--- src/oct2025/day1023/BOJ/dice14499/Main.java
+++ src/oct2025/day1023/BOJ/dice14499/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -23,9 +24,9 @@ public class Main {
 		
 		//지도 그리기
 		int[][] map = new int[N][M];
-		for(int i=0; i<M; i++) {
+		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
+			for(int j=0; j<M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
@@ -45,27 +46,54 @@ public class Main {
 		 *   6
 		 */
 		int[] dice = new int[7];
-		int start = 1;
 		
 		for(int i=0; i<K; i++) {
 			int nowOrder = order.poll();
 			int nextX = x + dr[nowOrder];
 			int nextY = y + dc[nowOrder];
-			
-			if(nextX >= 0 && nextX < M && nextY >= 0 && nextY < N) {
+//			System.out.println(nextX + " " + nextY);
+			if(nextX >= 0 && nextX < N && nextY >= 0 && nextY < M) {
 				x = nextX;
 				y = nextY;
+				//맨 윗 값은 공통
+				int tmp = dice[1];
+				//명령에 따라 값을 스위칭
+				switch (nowOrder) {
+				case 1:
+					dice[1] = dice[4];
+					dice[4] = dice[6];
+					dice[6] = dice[3];
+					dice[3] = tmp;
+					break;
+				case 2:
+					dice[1] = dice[3];
+					dice[3] = dice[6];
+					dice[6] = dice[4];
+					dice[4] = tmp;
+					break;
+				case 3:
+					dice[1] = dice[5];
+					dice[5] = dice[6];
+					dice[6] = dice[2];
+					dice[2] = tmp;
+					break;
+				case 4:
+					dice[1] = dice[2];
+					dice[2] = dice[6];
+					dice[6] = dice[5];
+					dice[5] = tmp;
+					break;
+				}
+				//바닥면 값을 이동한 좌표값으로 변경
+				if(map[x][y] == 0) {
+					map[x][y] = dice[6];
+				} else {
+					dice[6] = map[x][y];
+					map[x][y] = 0;
+				}
+
+				System.out.println(dice[1]);
 			}
-			
-			
-			
-			
-			
-			
-			
 		}
-		
-		
-		
 	}
 }
