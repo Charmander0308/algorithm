fa5ec50a290f8849bf265e9bc6184b4c5a834cff
yesung han <yesunghan@yesungui-MacBookPro.local>
2025-11-15 01:11:13 +0900

    20251115home

--- src/btypeprep/week1/no1_insomnia/Solution.java
+++ src/btypeprep/week1/no1_insomnia/Solution.java
@@ -1,5 +1,40 @@
 package btypeprep.week1.no1_insomnia;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+	static boolean[] isVisited;
+	static int cnt;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int test_case=1; test_case<=T; test_case++) {
+			int N = sc.nextInt();
+			int k = 1;
+			isVisited = new boolean[10];
+			cnt = 0;
+			while(cnt < 10) {
+				long num = N*k;
+				function(num);
+				k++;
+			}
+			System.out.println("#" + test_case + " " + N*(k-1));
+		}
+	}
+
+	private static void function(long num) {
+		int n = (int)num % 10;
+		if(isVisited[n] == false) {
+			isVisited[n] = true;
+			cnt++;
+		}
+		
+		if(num < 10) return;
+		
+		function(num / 10);
+	}
+	
+	
 }
--- src/btypeprep/week1/no2_binary/Solution.java
+++ src/btypeprep/week1/no2_binary/Solution.java
@@ -1,5 +1,27 @@
 package btypeprep.week1.no2_binary;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int test_case=1; test_case<=T; test_case++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			
+			boolean b = true;
+			
+			for(int i=0; i<N; i++) {
+				if((M & (1<<i)) == 0) {
+					b = false;
+					break;
+				}
+			}
+			
+			System.out.println("#" + test_case + " " + (b ? "ON" : "OFF"));
+			
+		}
+	}
 }
